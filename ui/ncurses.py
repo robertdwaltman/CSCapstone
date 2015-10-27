4d786e93b5e77874ab27483d5fa3de43e7fe6e7c
Robert Waltman <waltmanr@flip2.engr.oregonstate.edu>
2015-10-27 15:32:06 -0700

    added secondary window and user input

--- ui/ncurses.py
+++ ui/ncurses.py
@@ -8,8 +8,15 @@ class NCurses(object):
 
         # create border and add string
         self.stdscr.border(0)
-        self.stdscr.addstr(12, 25, "cs419 - Group 5 (Press any key to exit UI)")
+        self.queryBox = curses.newwin(10, 50, 12, 0)
+        self.queryBox.border(0)
+        self.stdscr.addstr(11, 25, "cs419 - Group 5")
+        self.queryBox.addstr(1, 5, "Enter your query below:")
         self.stdscr.refresh()
+        inputString = self.queryBox.getstr(3, 25)
+        self.queryBox.addstr(5, 5, "You entered: %s" % inputString)
+        self.queryBox.addstr(6, 5, "(Press any key to exit UI)")
+        self.queryBox.refresh()
 
         # Wait for user input and then quit
         self.stdscr.getch()
